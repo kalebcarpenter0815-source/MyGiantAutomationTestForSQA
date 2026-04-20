f25d5976190ccaffacf9c8e8910438bfa67eca96
kalebcarpenter0815-source <kaleb.carpenter0815@stu.mtec.edu>
2026-04-20 16:41:06 -0600

    Final Commit with updated fixes

--- repo_history_bundle/snapshots/01_02599b0_first_automation_file_for_committing/test/pageobjects/pagecopy1.js
+++
@@ -1,7 +0,0 @@
-import { browser } from '@wdio/globals'
-
-export default class signingInPage {
-    open (path) {
-        return browser.url(`https://www.saucedemo.com/`)
-    }
-}
--- repo_history_bundle/snapshots/05_b8d6973_Almost_there_commit/test/pageobjects/thesigningInPage.js
+++
@@ -1,7 +0,0 @@
-import { browser } from '@wdio/globals'
-export default class signingInPage {
-    async open() {
-        await browser.url(`https://www.saucedemo.com/`)
-        return this;
-    }
-}
